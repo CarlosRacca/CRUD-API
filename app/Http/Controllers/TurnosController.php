5f9f489b897dfc7a8a641d37d9fcd4de89a16f09
Carlos Racca <carlosracca1@gmail.com>
2022-08-22 21:37:25 -0300

    repo completo

---
+++ app/Http/Controllers/TurnosController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\turnos;
+
+class TurnosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $turnos = Turnos::all();
+
+        return $turnos;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $turnos =  new Turnos();
+        $turnos->date = $request->date;
+        $turnos->time = $request->time;
+        $turnos->customer = $request->customer;
+
+        $turnos->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $turnos =  Turnos::findOrFail($request->id);
+        $turnos->date = $request->date;
+        $turnos->time = $request->time;
+        $turnos->customer = $request->customer;
+
+        $turnos->save();
+
+        return $turnos;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request,$id)
+    {
+        $turnos = Turnos::destroy($request->id);
+
+        return $turnos;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TurnosController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+//Rutas para el crud de los turnos
+Route::get('/turnos', [TurnosController::class, 'index']);
+Route::post('/turnos', 'App\Http\Controllers\TurnosController@store');
+Route::put('/turnos/{id}', 'App\Http\Controllers\TurnosController@update');
+Route::delete('/turnos/{id}', 'App\Http\Controllers\TurnosController@destroy');
